c9f47b0fa0bfe3c063c391bbce19cb9de0ae7b99
Yukai Huang <yukaihuangtw@gmail.com>
2026-01-11 21:08:11 -0800

    feat: support horiziontal scroll

--- src/normal.c
+++ src/normal.c
@@ -87,6 +87,8 @@ struct input_event *normal_mode(struct input_event *start_ev, int oneshot)
 		"right",
 		"screen",
 		"scroll_down",
+		"scroll_left",
+		"scroll_right",
 		"scroll_up",
 		"start",
 		"top",
@@ -152,6 +154,22 @@ struct input_event *normal_mode(struct input_event *start_ev, int oneshot)
 			if (ev->pressed) {
 				scroll_stop();
 				scroll_accelerate(SCROLL_UP);
+			} else
+				scroll_decelerate();
+		} else if (config_input_match(ev, "scroll_left")) {
+			redraw(scr, mx, my, 1);
+
+			if (ev->pressed) {
+				scroll_stop();
+				scroll_accelerate(SCROLL_LEFT);
+			} else
+				scroll_decelerate();
+		} else if (config_input_match(ev, "scroll_right")) {
+			redraw(scr, mx, my, 1);
+
+			if (ev->pressed) {
+				scroll_stop();
+				scroll_accelerate(SCROLL_RIGHT);
 			} else
 				scroll_decelerate();
 		} else if (config_input_match(ev, "accelerator")) {
